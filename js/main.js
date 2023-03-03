c177710ebb1e84d7e9163aeba8e93b7e044afccf
silentazure <cd17@iu.edu>
2023-03-02 22:03:24 -0500

    Site updated: 2023-03-02 22:03:24

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $(() => {
   $('[data-toggle="tooltip"]').tooltip();
 
   // 代码高亮
-  document.querySelectorAll("pre code").forEach((block) => {
-    hljs.highlightBlock(block);
-  });
+  //document.querySelectorAll("pre code").forEach((block) => {
+  //  hljs.highlightBlock(block);
+  //});
 
 });
